877d061504a734b1ab745e85c611d7b0e723908c
Fazliddin <99480480+FazliddinMirzaqosimov@users.noreply.github.com>
2023-10-29 17:18:36 +0500

    Update Index.jsx

--- src/pages/CongratsPage/Index.jsx
+++ src/pages/CongratsPage/Index.jsx
@@ -8,22 +8,24 @@ export default function CongratsPage() {
   return (
     <div className='Congrats'  >
       <div className="MainInfo">
-        <h1>Ваша заявка успешно отправлена!</h1>
-        <p>Работаем без выходных с 9:00 до 21:00.
-          По любым вопросам можете обращаться:</p>
+        <h1>Arizangiz muvaffaqiyatli yuborildi
+!</h1>
+        <p>Biz haftada etti kun 9:00 dan 21:00 gacha ishlaymiz.
+          Barcha savollar bo'yicha quyidagi manzilga murojaat qilishingiz mumkin:
+</p>
       </div>
       <div className="footerInfo">
         <p>Связаться с менеджером</p>
         <div className="phoneFooter">
           <img src={phone} alt="" />
-          <span>+998 90 009 6009</span>
+          <span>+998 55 515 30 30</span>
         </div>
       </div>
-      <div className="networksIcon">
+{/*       <div className="networksIcon">
         <img src={insta} alt="" />
         <img src={telegram} alt="" />
         <img src={facebook} alt="" />
-      </div>
+      </div> */}
     </div>
   )
 }
