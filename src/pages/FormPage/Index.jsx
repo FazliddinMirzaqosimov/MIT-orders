9083f9f7df12f18e8f4dd16f23092cc982c045fe
Fazliddin <99480480+FazliddinMirzaqosimov@users.noreply.github.com>
2023-10-29 17:20:05 +0500

    Update Index.jsx

--- src/pages/FormPage/Index.jsx
+++ src/pages/FormPage/Index.jsx
@@ -7,7 +7,7 @@ import pochta from '../../icons/pochta.svg'
 import { InputMask } from 'primereact/inputmask';
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import {Link} from "react-router-dom
+import {Link} from "react-router-dom"
 
 export default function FormPage() {
   const navigate = useNavigate()
